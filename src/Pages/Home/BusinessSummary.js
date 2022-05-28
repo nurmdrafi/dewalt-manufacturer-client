01aa8162ce4ce8b3a19f3a1b4103a11a219aa2de
nurmdrafi <nurmdrafi2014@gmail.com>
2022-05-28 18:59:36 +0600

    react counter fix rendering issue

--- src/Pages/Home/BusinessSummary.js
+++ src/Pages/Home/BusinessSummary.js
@@ -8,25 +8,25 @@ const BusinessSummary = () => {
       <h2 className="text-center font-bold text-3xl my-16">Business Summary</h2>
       <div className="grid lg:grid-cols-3 grid-cols-1 gap-8">
         {/* Customers */}
-        <div className="card min-w-[300px] bg-base-100 shadow-xl mx-auto">
+        <div className="card w-[300px] bg-base-100 shadow-xl mx-auto">
           <div className="card-body">
             <h2 className="text-4xl font-bold text-center">
-              <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+              <VisibilitySensor partialVisibility offset={{ bottom: 0 }}>
                 {({ isVisible }) => (
                   <div style={{ height: 50 }}>
                     {isVisible ? <CountUp end={1000} /> : null}+
                   </div>
                 )}
               </VisibilitySensor>
-              <h2 className="text-4xl font-bold text-green-500">Customers</h2>
+              <h2 className="text-3xl font-bold text-green-500">Customers</h2>
             </h2>
           </div>
         </div>
         {/* Annual Revenue */}
-        <div className="card min-w-[300px] bg-base-100 shadow-xl mx-auto">
+        <div className="card w-[300px] bg-base-100 shadow-xl mx-auto">
           <div className="card-body">
             <h2 className="text-4xl font-bold text-center">
-              <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+              <VisibilitySensor partialVisibility offset={{ bottom: 0 }}>
                 {({ isVisible }) => (
                   <div style={{ height: 50 }}>
                     {isVisible ? <CountUp end={120} /> : null}M+
@@ -34,23 +34,23 @@ const BusinessSummary = () => {
                 )}
               </VisibilitySensor>
             </h2>
-            <h2 className="text-4xl font-bold text-green-500">
+            <h2 className="text-3xl font-bold text-green-500">
               Annual Revenue
             </h2>
           </div>
         </div>
         {/* Tools */}
-        <div className="card min-w-[300px] bg-base-100 shadow-xl mx-auto">
+        <div className="card w-[300px] bg-base-100 shadow-xl mx-auto">
           <div className="card-body">
             <h2 className="text-4xl font-bold text-center">
-              <VisibilitySensor partialVisibility offset={{ bottom: 200 }}>
+              <VisibilitySensor partialVisibility offset={{ bottom: 0 }}>
                 {({ isVisible }) => (
                   <div style={{ height: 50 }}>
-                    {isVisible ? <CountUp end={50} /> : null}+
+                    {isVisible ? <CountUp start={0} end={50} /> : null}+
                   </div>
                 )}
               </VisibilitySensor>
-              <h2 className="text-4xl font-bold text-green-500">Tools</h2>
+              <h2 className="text-3xl font-bold text-green-500">Tools</h2>
             </h2>
           </div>
         </div>
